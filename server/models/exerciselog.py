c1dad1b3f444a17805a7c201e02db980bf671cae
homeira kalim <homeirakalim@gmail.com>
2025-05-02 16:01:51 -0400

    workout session model complete

---
+++ server/models/exerciselog.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+
+
+from config import db
+
+# Models go here!
---
+++ server/models/workoutsession.py
@@ -0,0 +1,42 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData, DateTime
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+
+#!testcomment
+#todo testcomment
+#? testcomment
+#* testcomment
+
+from config import db
+
+# Models go here!
+# columns. validaions, and relationships
+class WorkoutSession(db.Model):
+    __tablename__='workout_sessions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    # ? do I need to add a validation here to prevent old out of date dates?
+    date = db.Column(db.DateTime, nullable=False)
+    # table-level validation (can't be empty/null)
+    name = db.Column(db.String(20), nullable=False)
+    #! Relationships
+    exercise_logs = db.relationship("ExerciseLog", back_populate="workout_sessions", cascade="all, delete")
+    serialize_rules = ("-exercise_logs.workoutsessions")
+
+    @validates('name')
+    def validate_name(self, key, value):
+        # if user enters extra spaces, .strip will delete the extra spaces before or after the name
+        if not value or len(value.strip()) == 0:
+            # ! check back re toast
+            raise ValueError("Workout title cannot be empty")
+        return value.strip()
+    
+
+
+# M.
+# validations:
+# model-level
+# table-level
+# form-level
